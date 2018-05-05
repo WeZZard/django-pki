8b6f08a9d164eddd631a45c3ba375a53bb48c78b
WeZZard <WeZZardLau@gmail.com>
2018-05-05 12:51:50 +0800

    Fixes typos in the app's urls.

--- django_pki/urls.py
+++ django_pki/urls.py
@@ -15,22 +15,22 @@ urlpatterns = [
     path('crl/', Index.as_view(), name='crls'),
 
     path('private-key/add', Index.as_view(), name='private_key_add'),
-    path('private-key/<id:pk>', Index.as_view(), name='private_key_update'),
-    path('private-key/<id:pk>/delete', Index.as_view(), name='private_key_delete'),
+    path('private-key/<int:pk>', Index.as_view(), name='private_key_update'),
+    path('private-key/<int:pk>/delete', Index.as_view(), name='private_key_delete'),
 
     path('csr/add', Index.as_view(), name='csr_add'),
-    path('csr/<id:pk>', Index.as_view(), name='csr_update'),
-    path('csr/<id:pk>/delete', Index.as_view(), name='csr_delete'),
+    path('csr/<int:pk>', Index.as_view(), name='csr_update'),
+    path('csr/<int:pk>/delete', Index.as_view(), name='csr_delete'),
 
     path('cert/add', Index.as_view(), name='cert_add'),
-    path('cert/<id:pk>', Index.as_view(), name='cert_update'),
-    path('cert/<id:pk>/revoke', Index.as_view(), name='cert_revoke'),
-    path('cert/<id:pk>/delete', Index.as_view(), name='cert_delete'),
+    path('cert/<int:pk>', Index.as_view(), name='cert_update'),
+    path('cert/<int:pk>/revoke', Index.as_view(), name='cert_revoke'),
+    path('cert/<int:pk>/delete', Index.as_view(), name='cert_delete'),
     path('cert/crl', Index.as_view(), name='cert_crl_index'),
-    path('cert/<id:pk>/crl', Index.as_view(), name='cert_crl_update'),
-    path('cert/<id:pk>/ocsp', Index.as_view(), name='cert_ocsp_update'),
+    path('cert/<int:pk>/crl', Index.as_view(), name='cert_crl_update'),
+    path('cert/<int:pk>/ocsp', Index.as_view(), name='cert_ocsp_update'),
 
     path('crl/add', Index.as_view(), name='crl_add'),
-    path('crl/<id:pk>', Index.as_view(), name='crl_update'),
-    path('crl/<id:pk>/delete', Index.as_view(), name='crl_delete'),
+    path('crl/<int:pk>', Index.as_view(), name='crl_update'),
+    path('crl/<int:pk>/delete', Index.as_view(), name='crl_delete'),
 ]
