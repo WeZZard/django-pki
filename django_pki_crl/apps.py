15bd0ba4e457701d139522c76f2fec5ec6960e8f
WeZZard <WeZZardLau@gmail.com>
2018-05-10 11:58:58 +0800

    Adds apps: django_pki_crl and django_pki_ocsp.

---
+++ django_pki_crl/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoPkiCrlConfig(AppConfig):
+    name = 'django_pki_crl'
---
+++ django_pki_ocsp/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class DjangoPkiOcspConfig(AppConfig):
+    name = 'django_pki_ocsp'
